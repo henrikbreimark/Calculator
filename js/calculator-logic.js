fade69b249179f6ab4d8083f0770a2bdb8d9cbc6
Henrik Breimark <henrik.breimark@gmail.com>
2022-11-12 21:29:32 +0100

    add the math-logic

---
+++ js/calculator-logic.js
@@ -0,0 +1,16 @@
+function add (numberA, numberB) {
+    return numberA+numberB;
+}
+
+function add (numberA, numberB) {
+    return numberA-numberB;
+}
+
+function multi (numberA, numberB) {
+    return numberA*numberB;
+}
+
+function div (numberA, numberB) {
+    return numberA/numberB;
+}
+
